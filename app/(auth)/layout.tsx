3012643937328f4b4e2268406cf365d084284f10
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-23 06:34:03 +0000

    clerk auth setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
