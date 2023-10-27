12fe5fd7d54d461f7d2999b2014efb2bc87aa20b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-27 14:34:51 +0000

    theme fixes

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const Topbar = () => {
   const router = useRouter();
@@ -32,6 +33,7 @@ const Topbar = () => {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organisationSwitcherTrigger: "py-2 px-4",
             },
