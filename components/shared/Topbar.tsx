f1c5c9a805bc996cd9f65abc2d3d9cc525c7153f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-27 14:00:18 +0000

    topbar basic styling implemented

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,6 +9,31 @@ const Topbar = () => {
         <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organisationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
     </nav>
   );
 };
