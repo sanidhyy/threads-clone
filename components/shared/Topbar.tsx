396091a3520ac452e47ea00e0f04b327a708ee51
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-27 13:54:23 +0000

    assets added

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Topbar = () => {
-  return <h1>Topbar</h1>;
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+    </nav>
+  );
 };
 
 export default Topbar;
